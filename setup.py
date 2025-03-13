01465d7edd35feab998f214e50eef53edc266d25
Reda A <113383931+azaidr@users.noreply.github.com>
2025-03-13 14:10:24 -0400

    Logistic PCA

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="logisticpca",
-    version="0.1",
+    name="logisticpca", 
+    version="0.1.7",  
     packages=find_packages(),
-    install_requires=["torch", "numpy"],
+    install_requires=[
+        "torch",
+        "numpy"
+    ],
     author="Reda Abouzaid",
     author_email="azaidr00@gmail.com",
-    description="A Python implementation of PCA for binary data.",
+    description="A Python implementation of Logistic PCA for binary data.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourgithub/logisticpca",
+    url="https://github.com/azaidr/logisticpca",  
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
